e5e0fb53c12a74df5919ffaa1b75fcf40e1bc103
Manuel Olvera <manuel.olvera@makepositive.com>
2012-12-12 20:29:22 +0000

    learning english

---
+++ app/controller/Main.js
@@ -0,0 +1,128 @@
+Ext.define('LearningEnglish.controller.Main', {
+	extend: 'Ext.app.Controller',
+
+	init : function(){
+
+		document.addEventListener("backbutton",
+			function(){
+				if( Ext.Viewport.getActiveItem().getActiveItem().xtype === 'main_panel'
+                    ||  Ext.Viewport.getActiveItem().getActiveItem().xtype === 'login_panel' ){
+					// back on home page must quit the app
+					navigator.app.exitApp();
+				}else{
+					history.back();
+				}
+			}
+
+		, false);
+	},
+
+	config: {
+		refs: {
+
+			salesforceSingInButton : '#login_sing_in',
+            getWordsButton : '#main_get_words',
+			// tabpanel buttons - for history support as well
+			// homeTabBarButton    : 'tabbar button[title=Home]',
+
+		},
+		control: {
+
+			// homeTabBarButton    : {
+			// 	tap: function(){
+			// 		this.redirectTo('home');
+			// 	}
+			// }
+			salesforceSingInButton : {
+				tap: 'salesforceSingIn'
+			},
+            getWordsButton : {
+                tap: function() {
+                    // '_authenticate'
+                    this._authenticate('getWords', function(){console.log('errorCallback')});
+                }
+            }
+		},
+
+		// history support
+		routes: {
+
+			// 'home'		 		: 'showHome'
+
+		}
+	},
+
+	salesforceSingIn: function() {
+
+    var mainController = this;
+    console.log("login called");
+
+    // Instantiating forcetk ClientUI
+    ftkClientUI = new forcetk.ClientUI(LearningEnglish['loginURL'], LearningEnglish['consumerKey'],
+        LearningEnglish['callbackURL'], LearningEnglish['proxy'],
+        function forceOAuthUI_successHandler(forcetkClient) { // successCallback
+            console.log('OAuth success!');
+            console.log('forcetkClient', forcetkClient);
+            LearningEnglish['sfdcClient)'] = ftkClientUI.client;    
+            console.log('LearningEnglish[\'sfdcClient)\']', LearningEnglish['sfdcClient)']);
+            // Initialize the main view
+            Ext.Viewport.add(Ext.create('LearningEnglish.view.Main'));
+            Ext.Viewport.setActiveItem(1);
+        },
+        function forceOAuthUI_errorHandler(error) { // errorCallback
+            console.log('error', error);
+            alert('OAuth error!');
+        }
+    );
+
+    // Initiating login process
+    ftkClientUI.login();
+
+	},
+
+    getWords: function() {
+
+        console.log('getWords starts');
+            console.log('LearningEnglish[\'sfdcClient)\']', LearningEnglish['sfdcClient)']);
+
+        LearningEnglish['sfdcClient'].query("SELECT English__c, Spanish__c FROM Word__c",
+            function(response){
+                console.log('getWords Callback');
+                console.log('response', response);
+                for(i = 0; i < response['records'].length; i++){
+                    console.log('Word '+ i +': '+response.records[i].English__c);
+                }
+            }
+        );
+
+    },
+
+    _authenticate: function(callback, errorCallback) {
+
+        console.log('_authenticate starts');
+        var refreshToken = localStorage.getItem('ftkui_refresh_token');
+        var mainController = this;
+
+        console.log('refreshToken',refreshToken);
+        // Initializes the sfdc client
+        LearningEnglish['sfdcClient'] = new forcetk.Client(LearningEnglish['consumerKey'],
+                                            LearningEnglish['loginURL'], LearningEnglish['proxy']);
+        LearningEnglish['sfdcClient'].setRefreshToken(refreshToken);
+
+        console.log('LearningEnglish[\'sfdcClient\']',LearningEnglish['sfdcClient']);
+
+        LearningEnglish['sfdcClient'].refreshAccessToken(
+            function refreshAccessToken_successHandler(sessionToken) {
+                LearningEnglish['sfdcClient'].setSessionToken(sessionToken.access_token, null, sessionToken.instance_url);
+                console.log('INFO: OAuth login successful!')
+                console.log('callback', callback);
+                mainController[callback].call();
+            },
+            function refreshAccessToken_errorHandler(jqXHR, textStatus, errorThrown) {
+                console.log('ERROR: OAuth login!')
+                errorCallback.call();
+            }
+        );
+    }
+
+});
